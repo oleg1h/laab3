d021b063c77d82779f7b6150439b6794bb1e0e3b
Oleh <tzymbala.oleg@gmail.com>
2025-11-06 21:09:33 +0200

    New Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+ 
+
+namespace MyTeamCityProject
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Hello, TeamCity!");
+
+        }
+
+    }
+
+}
