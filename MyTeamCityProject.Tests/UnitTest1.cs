0c2c7f637fd6b64256756664f22f053580eb0285
Oleh <tzymbala.oleg@gmail.com>
2025-11-07 15:02:24 +0200

    new

---
+++ MyTeamCityProject.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace MyTeamCityProject.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void True_ShouldBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
